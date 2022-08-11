cfe45b6e9a52f176cf34fa9897a6a6445d87d284
zsheperdigian <zsheperdigian@gmail.com>
2022-08-10 21:36:47 -0700

    completed writing sudo code for js, the main outline is layed out in the html

---
+++ assets/script.js
@@ -0,0 +1,62 @@
+// global varibales
+
+
+    //local storage variable
+
+//DOM variables
+
+
+
+
+// API variables
+// moment -current time
+// moment - current date
+
+
+//create function to take in user input for city
+          // -uses API to grab current weather conditions
+          // - uses API to grab future weather conitions
+          // - stores city in local storage
+          // - appends city to search hisotry ("".searchbtn") (creates a btn that pulls up results of city)
+
+
+//create function to search city stored in history when clicked
+    //create btns for each searched city
+        //start function that gets city info
+    //put the newest searches on top
+    // pull from local storage
+
+
+// create function to populate main city weather info: pull...
+    // -city name, 
+    //  -current date
+    //  -current weather condition (link to icon)
+        //create element that holds info
+        
+
+//create fuction to populate main city extra info:
+    //  -temp
+    //  -humidity
+    //  -wind speed
+    //  -uv index
+
+
+//create function to color code uv index
+    //if (UV index < x) {
+        //set color to green (favorable)
+    //}
+    //if (x < UV index < y) {
+        //set color to yellow (moderate)
+    //}
+    //if (UV index > y) {
+        //set color to red (severe)
+    //}
+    //return 
+
+
+
+//create function that loops through city info and posts the 5 day forcast
+    //grab global variables for each of the next days
+    // display date, icon representing weather condition, temp, windspeed and humidity
+
+
