b726f07e5dd8686f4031888de3d508c907e61cb5
zsheperdigian <zsheperdigian@gmail.com>
2022-08-14 17:02:27 -0700

    compelted function to use API to graph lat and lon from name

--- assets/script.js
+++ assets/script.js
@@ -6,91 +6,107 @@ var weatherApiKey = "33267835088a1301ccfa970b21fd1522";
 //DOM variables
 var userCity =$("#cname"); // city name from user
 var searchForm =$("#search"); // form container for search
-var cityHisory =$("#savedCities");
+var searchHistory =$("#savedcities"); // search hisory box
 //make variable to reference city weather info
 // make variable to reference 5day forcast
 
 
 //variables
-var cityHistory = []; // empty city 
+var cityHistory = []; // empty city list
 // moment -current time
 // moment - current date
 // API variables
 
 
-//create function to take in user input for city
-           // -use city to grab lat and lon
-
-console.log(userCity);
-
+//Action code:
+showHistory();
 
 // function that loads the saved cities from local storage
 function showHistory () {
     var savedCities = localStorage.getItem("Saved-Cities");
+
     if (savedCities) {
         savedCities = JSON.parse(savedCities);
-        cityHisory.innerHTML = '';
+        console.log(savedCities);
+        searchHistory.innerHTML = ''; //wipes clean to prevent stacking
     for (var i = savedCities.length - 1; i >= 0; i--) {
-        var btn = document.createElement('button');
+        let btn = document.createElement("button");
         btn.setAttribute('type', 'button');        
         btn.textContent = savedCities[i];
         btn.setAttribute('data-search', savedCities[i]);
-        cityHisory.append(btn);
+        searchHistory.append(btn);
+        console.log(btn);
     }
     } else return;
 };
-
 
 
 //function that searches API when search city is clicked
 $("#searchBtn").click(function(event){
     event.preventDefault();
     // prevent empty value of city
-    var chosenCity = userCity.value;
-    if (!chosenCity.value){
+    var chosenCity = userCity.val(); // grabs user's entered city name
+    if (!chosenCity){
         console.log("there was nothing entered");
         return;
     }
     console.log(chosenCity);
     getCity(chosenCity); //gets the lat/lon of requested city
-    // add it to history and save to local storage
-    localStorage.setItem("Saved-Cities", JSON.stringify(cityHisory));
-
-    //rerun search history with new event
-
-    //rempty usercity
-    userCity.value = "";
+    cityHistory.push(chosenCity); // adds to the city history array
+       
+    localStorage.setItem("Saved-Cities", JSON.stringify(cityHistory)); //adds to local storage
+    
+    showHistory(); //rerun search history with new event
+    
+   
 });
     
-
-
-
-
-
-
 
 
 //function to get cities lat and lon values via name
 function getCity(uc) {
     var citylocation = `http:api.openweathermap.org/geo/1.0/direct?q=${uc}&limit=1&appid=${weatherApiKey}`;
 
-    console.log(uc);
+    
     fetch(citylocation) // using variable name to grab coordinates
-        .then((response) => response.json())
-        .then((data) => console.log(data[0]));
+        .then((response) => {
+            return response.json();
+        })
+
+        .then(function (data) {
+            const {name} = data;            
+            const {dt} = data;
+            const {timezone} = data;
+            var adjustedTime = timezone / 60; 
+            var formattedTime = moment.unix(dt).utc().utcOffset(adjustedTime).format('MM/DD/YYYY');          
+            const {temp} = data.main;
+            const {speed} = data.wind;
+            const {humidity} = data.main;
+            const {lat} = data.coord;
+            const {lon} = data.coord;
+            $('#currentHumid').text(humidity+"%");
+            $("#currentCityName").text(name);
+            $("#cityDateTime").text(formattedTime);
+            $("#currentTemp").text(temp + "°F");
+            $('#currentWindSpeed').text(speed+" MPH"); 
+            $('#weatherIcon').attr("src",'https://openweathermap.org/img/wn/'+ icon +'.png') ;      
+            return getWeather(lat, lon);
+        
+        });
 }
 
+function getWeather(latitude, longitude) {
 
 
-//create function to prevent default on form
-function fhandle(event) {
-    //prevent no submission
-    
-    
-    userCity.value = ""; // re-empty the input section
+
+
+
+
+
 
 
 }
+
 
 
           // -uses API to grab current weather conditions
@@ -99,15 +115,7 @@ function fhandle(event) {
           // - appends city to search hisotry ("".searchbtn") (creates a btn that pulls up results of city)
 
 
-//create function to search city stored in history when clicked
-function cityHistory() {
-    
-    //create btns for each searched city
-        //start function that gets city info
-    //put the newest searches on top
-    // pull from local storage
 
-}
 
 // create function to populate main city weather info: pull...
     // -city name, 
